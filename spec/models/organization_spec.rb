4f747dc0a7e703a55a64daa042b834831940e927
MadsNeis <neismads@gmail.com>
2026-01-25 16:44:45 -0800

    Attribute test added for organization_spec

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -4,4 +4,19 @@ RSpec.describe Organization, type: :model do
 it "exists" do
     Organization.new
   end 
+
+  describe 'attributes' do
+    it {should respond_to(:name)}
+    it {should respond_to(:status)}
+    it {should respond_to(:phone)}
+    it {should respond_to(:email)}
+    it {should respond_to(:description)}
+    it {should respond_to(:rejection_reason)}
+    it {should respond_to(:liability_insurance)}
+    it {should respond_to(:primary_name)}
+    it {should respond_to(:secondary_name)}
+    it {should respond_to(:secondary_phone)}
+    it {should respond_to(:title)}
+    it {should respond_to(:transportation)}
+  end
 end

7b1abc0b77a3702cfe76d26079cb234294d079bd
MadsNeis <neismads@gmail.com>
2026-01-25 16:40:01 -0800

    Attribute tests for ticket added.

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -4,4 +4,16 @@ RSpec.describe Ticket, type: :model do
   it "exists" do
     Ticket.new
   end 
+
+  describe 'attributes' do
+    it {should respond_to(:name)}
+    it {should respond_to(:description)}
+    it {should respond_to(:phone)}
+    it {should respond_to(:organization_id)}
+    it {should respond_to(:closed)}
+    it {should respond_to(:closed_at)}
+    it {should respond_to(:resource_category_id)}
+    it {should respond_to(:region_id)}
+  end
+
 end

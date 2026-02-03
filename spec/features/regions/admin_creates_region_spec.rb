de2f21ef3df81f74a205b0bf6b434a5fb0a6b057
Denise <deltafoxtrot27@outlook.com>
2026-02-02 17:57:34 -0800

    working on goal 8

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
-
+  scenario "successfully creates a region" do
+    admin = create(:user, role: "admin")
+    log_in_as(admin)
+    visit regions_path
+    click_link "new region"
+    fill_in "Name", with: "Central Oregon"
+    click_button "Create Region"
+    expect(page).to have_content("Central Oregon")
+  end
 end
